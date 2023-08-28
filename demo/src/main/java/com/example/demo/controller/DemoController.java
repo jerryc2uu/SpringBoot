a7770fdc4f04d6aee555da2478a7cd1799b4b5a5
aknod <aknod@psy>
2023-08-28 12:54:33 +0900

    Second

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,5 +1,28 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.DemoService;
+
+@RestController
 public class DemoController {
+
+	@Autowired
+	private DemoService demoService;
+
+	@GetMapping("/demo")
+	public String getDemo() {
+		String demo = demoService.getDemo();
+		return demo;
+	}
+
+	@PostMapping("/demo")
+	public String postDemo() {
+		String demo = demoService.getDemo();
+		return demo;
+	}
 
 }
